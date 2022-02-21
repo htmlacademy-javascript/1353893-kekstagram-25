4be3292d069d8e449816af5a999c1710ff434f35
Владимир Арутюнов <zlobajs@yandex.ru>
2022-02-21 19:02:12 +0400

    Исправлено домашнее задание

--- js/main.js
+++ js/main.js
@@ -1,42 +1,22 @@
 // Функция возвращает рандомное число.
 function randomInteger (min, max) {
-    let rand = min + Math.random() * (max + 1 - min);
-    if (min == max && max == min || min < 0 || max < 0) {
-        console.log('Поменяйте значение');
-    } else {
-        return Math.floor(rand);
-    }
-};
-
-console.log(randomInteger(-1, 100));
-
-
-/*
-function hello (str, maxValue) {
-    if (str.length >= maxValue) {
-        return console.log('Вы не можете написать комментарий');
-    }
-        return console.log('Вы можете написать комментарий');
+  const rand = min + Math.random() * (max + 1 - min);
+  if (min === max || min < 0 || max < 0) {
+    return false;
+  }
+  return Math.floor(rand);
 }
-*/
 
-function getLength () {
-    if (str.length >= maxValue) {
-        return alert('Вы не можете написать комментарий');
-    }  
-        return alert('Вы можете написать комментарий');
-};
+randomInteger();
 
 
-function getInfoAlert() {
-     str = prompt("Введите текст " , "");
-     maxValue = prompt("Введите максимальную длину " , "");
-    if (str == undefined || str == "") {
-        alert("Строка не введена");
-    } else {
-         getLength();
-    }
-};
-getInfoAlert(getLength);
+function checkLength(str, maxValue) {
+  if (str.length > maxValue) {
+    return true;
+  }
+  return false;
+}
+
+checkLength();
 
 
